dc732f6ca360364858ba46fd20cf22be9148fad0
Frowingg <edbico0@gmail.com>
2022-06-17 15:45:13 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// creo l'object contenente le informazioni
+const infoMembers = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chied Editor',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Menager',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Menager'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+    },
+]
+console.log(infoMembers)
