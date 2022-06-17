2dc016dd675be1b7bad5b682b631f064e83cdfde
Frowingg <edbico0@gmail.com>
2022-06-17 16:09:58 +0200

    mamber

--- js/script.js
+++ js/script.js
@@ -1,28 +1,50 @@
+// inizializzo le variabili
+const teamContainer = document.querySelector('.team-container');
+
 // creo l'object contenente le informazioni
 const infoMembers = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
+        img:'img/wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
         role: 'Chied Editor',
+        img:'img/angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Menager',
+        img:'img/walter-gordon-office-manager.jpg'
     },
     {
         name: 'Angela Lopez',
-        role: 'Social Media Menager'
+        role: 'Social Media Menager',
+        img:'img/angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',
+        img:'img/scott-estrada-developer.jpg'
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
+        img:'img/barbara-ramos-graphic-designer.jpg'
     },
 ]
-console.log(infoMembers)
+
+
+for(let i = 0; i<=5; i++) {
+    teamContainer.innerHTML +=     
+    `<div class="team-card">
+        <div class="card-image">
+            <img src="${infoMembers[i].img}" alt="${infoMembers[i].name}" />
+        </div>
+        <div class="card-text">
+            <h3>${infoMembers[i].nome}</h3>
+            <p>${infoMembers[i].role}</p>
+        </div>
+    </div>`
+}
